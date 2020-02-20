c9575591625ae90140f94a90a703eebd446a2505
FedeIng <56321831+FedeIng@users.noreply.github.com>
2020-02-21 00:41:17 +0100

    Add files via upload

--- student.py
+++ student.py
@@ -51,8 +51,7 @@ def calculate_similarity(text1, text2):
     return base.similarity(compare)
 
 seed(datetime.now())
-dizionario=[]
-id_id={}
+array={}
 threadLock = threading.Lock()
 
 try:
@@ -77,24 +76,16 @@ class OutputThread (threading.Thread):
             if y[0] in dizionario:
                 print("La risposta "+y[0]+" é stata eliminata")
                 if y[1] =='BANNED':
-                    for elem in id_id[y[0]]:
+                    for elem in array[y[0]]['id']:
                         bot.sendMessage(elem, "La domanda '"+y[0]+"' é stata bannata")
                 else :
-                    for elem in id_id[y[0]]:
+                    for elem in array[y[0]]['id']:
                         bot.sendMessage(elem, "La risposta alla domanda '"+y[0]+"' e' '"+y[1]+"'")
-                with open('data.txt','r') as json_file:
-                    data = json.load(json_file)
-                    for i in range (0,len(data)):
-                        for p in data[str(i)] :
-                            if y[0] == p['q']:
-                                p['a']=y[1]
-                    json_file.close()
-                    with open('data.txt','w') as f:
-                        f.write(str(data).replace("'",'"'))
-                dizionario.remove(y[0])
-                del id_id[y[0]]
-                with open('id.txt','w') as f:
-                    f.write(str(id_id).replace("'",'"'))
+                for i in array:
+                    if y[0] == array[i]:
+                        array[i]['a']=y[1]
+                with open('data.txt','w') as f:
+                    f.write(str(array).replace("'",'"'))
             else :
                 print("Error: String not found")
             threadLock.release()
@@ -108,76 +99,67 @@ def on_chat_message(msg):
     num=0
     if content_type == 'text':
         threadLock.acquire()
-        with open('data.txt','r') as json_file:
-            data = json.load(json_file)
-            trovata = False
-            txt=msg['text'].lower()
-            if  txt == '/start' and req_type==0 :
-                req_type=3
-                trovata = True
-                bot.sendMessage(chat_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
-            elif  txt == '/question' and req_type==0 :
-                req_type=3
-                trovata = True
-                bot.sendMessage(chat_id, 'Digitare la domanda da inviare al prof o agli assistenti del corso:')
-                id_command[chat_id]=1
-            elif  txt == '/report' and req_type==0 :
-                req_type=3
-                trovata = True
-                bot.sendMessage(chat_id, 'Digitare il bug da segnalare al programmatore:')
-                id_command[chat_id]=2
-            elif chat_id in id_command:
-                if id_command[chat_id]==1 :
-                    req_type=1
-                    for i in range (0,len(data)):
-                        for p in data[str(i)] :
-                            string1=process_text(txt)
-                            string2=process_text(p['q'].replace("#","'").replace("$",'"'))
-                            num=calculate_similarity(string1,string2)
-                            if num>sim:
-                                sim=num
-                                val=i
-                    print(sim)
-                    if sim>0.8:
-                        for p in data[str(i)]:
-                            trovata = True
-                            if p['a'] == '':
-                                bot.sendMessage(chat_id, 'Domanda in attesa di risposta')
-                                if chat_id not in id_id[p['q']]:
-                                    id_id[p['q']].append(str(chat_id))
-                                    with open('id.txt','w') as f:
-                                        f.write(str(id_id).replace("'",'"'))
-                                elif p['a']=='BANNED':
-                                    bot.sendMessage(chat_id, 'La domanda da te fatta é stata bannata')
-                                else :
-                                    bot.sendMessage(chat_id, p['a'])
-                elif id_command[chat_id]==2 and req_type==0 :
-                    req_type=2
-                    with lock:
-                        with open('student.txt','a') as f:
-                            f.write("@@@"+txt+"###")
-                    bot.sendMessage(chat_id, 'Bug segnalato')
-                del id_command[chat_id]
-            else:
-                req_type=4
-                trovata = True
-                bot.sendMessage(chat_id, 'Comando non trovato')
-                bot.sendMessage(chat_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
-            if  not trovata and req_type==1:
-                json_file.close()
-                string='{"'+str(len(data))+'":[{"q":"'+txt.replace("'",'#').replace('"',"$")+'","a":""}]}'
-                data.update(eval(string))
-                with open('data.txt','w') as f:
-                    f.write(str(data).replace("'",'"'))
-                id_id[txt]=[]
-                id_id[txt].append(str(chat_id))
-                with open('id.txt','w') as f:
-                    f.write(str(id_id).replace("'",'"'))
-                dizionario.append(txt)
-                bot.sendMessage(chat_id, 'Risposta non trovata. Domanda inviata al professore')
-                msg=txt
-                s.send(msg.encode())
-            threadLock.release()
+        trovata = False
+        txt=msg['text'].lower()
+        if  txt == '/start' and req_type==0 :
+            req_type=3
+            trovata = True
+            bot.sendMessage(chat_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
+        elif  txt == '/question' and req_type==0 :
+            req_type=3
+            trovata = True
+            bot.sendMessage(chat_id, 'Digitare la domanda da inviare al prof o agli assistenti del corso:')
+            id_command[chat_id]=1
+        elif  txt == '/report' and req_type==0 :
+            req_type=3
+            trovata = True
+            bot.sendMessage(chat_id, 'Digitare il bug da segnalare al programmatore:')
+            id_command[chat_id]=2
+        elif chat_id in id_command:
+            if id_command[chat_id]==1 :
+                req_type=1
+                for i in array:
+                    string1=process_text(txt)
+                    string2=process_text(i.replace("#","'").replace("$",'"'))
+                    num=calculate_similarity(string1,string2)
+                    if num>sim:
+                        sim=num
+                        val=i
+                print(sim)
+                if sim>0.8:
+                    trovata = True
+                    if array[val]['a'] == '':
+                        bot.sendMessage(chat_id, 'Domanda in attesa di risposta')
+                        if chat_id not in array[val]['id']:
+                            array[val]['id'].append(str(chat_id))
+                            with open('data.txt','w') as f:
+                                f.write(str(array).replace("'",'"'))
+                    elif array[val]['a']=='BANNED':
+                        bot.sendMessage(chat_id, 'La domanda da te fatta é stata bannata')
+                    else :
+                        bot.sendMessage(chat_id, array[val]['a'])
+            elif id_command[chat_id]==2 and req_type==0 :
+                req_type=2
+                with lock:
+                    with open('student.txt','a') as f:
+                        f.write("@@@"+txt+"###")
+                bot.sendMessage(chat_id, 'Bug segnalato')
+            del id_command[chat_id]
+        else:
+            req_type=4
+            trovata = True
+            bot.sendMessage(chat_id, 'Comando non trovato')
+            bot.sendMessage(chat_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
+        if  not trovata and req_type==1:
+            array[txt]['id']=[]
+            array[txt]['id'].append(str(chat_id))
+            with open('data.txt','w') as f:
+                f.write(str(array).replace("'",'"'))
+            dizionario.append(txt)
+            bot.sendMessage(chat_id, 'Risposta non trovata. Domanda inviata al professore')
+            msg=txt
+            s.send(msg.encode())
+        threadLock.release()
 
 TOKEN = '1064330916:AAGjmjJZcEwyudWgPYplyP7OvyFQl4Ju_GI'
 ShellThread=OutputThread(s)
@@ -185,14 +167,7 @@ ShellThread.start()
 bot = telepot.Bot(TOKEN)
 
 with open('data.txt','r') as json_file:
-    data = json.load(json_file)
-    for i in range (0,len(data)):
-        for p in data[str(i)] :
-            if p['a'] == '':
-                dizionario.append(p['q'])
-
-with open('id.txt') as json_file:
-    id_id = json.load(json_file)
+    array=json.load(json_file)
 
 bot.message_loop({'chat':on_chat_message,'callback_query':on_callback_query})
 
--- teacher.py
+++ teacher.py
@@ -11,8 +11,7 @@ from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, Reply
 
 group_id=-1001143270084
 num=0
-dizionario=[]
-ban=[]
+array={}
 id_command={}
 threadLock = threading.Lock()
 question=""
@@ -27,8 +26,9 @@ def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor="callback_query")
     if query_data=='a':
         list1=[]
-        for elem in dizionario:
-            list1.append([elem])
+        for elem in array:
+            if array[elem]=="":
+                list1.append([elem])
         print(list1)
         keyboard1 = ReplyKeyboardMarkup(keyboard=list1)
         bot.sendMessage(group_id, 'Digitare il codice della domanda:',reply_markup=keyboard1)
@@ -40,31 +40,34 @@ def on_callback_query(msg):
         bot.sendMessage(group_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
     if query_data=='l':
         stringa=""
-        for elem in dizionario:
-            if stringa == "":
-                stringa=elem
-            else:
-                stringa+=",\n"+elem
+        for elem in array:
+            if array[elem]=="":
+                if stringa == "":
+                    stringa=elem
+                else:
+                    stringa+=",\n"+elem
         if stringa !="":
             bot.sendMessage(group_id, stringa)
         else:
             bot.sendMessage(group_id, "Lista vuota")
     if query_data=='b':
         list1=[]
-        for elem in dizionario:
-            list1.append([elem])
+        for elem in array:
+            if array[elem]=="":
+                list1.append([elem])
         print(list1)
         keyboard1 = ReplyKeyboardMarkup(keyboard=list1)
         bot.sendMessage(group_id, 'Digitare il codice della domanda:',reply_markup=keyboard1)
         id_command[group_id]=3
     if query_data=='bl':
         stringa=""
-        for elem in ban:
-            if stringa == "":
-                stringa=elem
-            else:
-                stringa+=",\n"+elem
-            print(stringa)
+        for elem in array:
+            if array[elem] == 'BANNED':
+                if stringa == "":
+                    stringa=elem
+                else:
+                    stringa+=",\n"+elem
+                print(stringa)
         if stringa !="":
             bot.sendMessage(group_id, stringa)
         else:
@@ -91,16 +94,18 @@ def on_chat_message(msg):
             bot.sendMessage(chat_id, 'Benvenuto nel bot di programmazione ad oggetti, selezionare un comando per usarlo', reply_markup=keyboard)
         elif txt=="/answer":
             list1=[]
-            for elem in dizionario:
-                list1.append([elem])
+            for elem in array:
+                if array[elem]=="":
+                    list1.append([elem])
             print(list1)
             keyboard1 = ReplyKeyboardMarkup(keyboard=list1)
             bot.sendMessage(group_id, 'Digitare il codice della domanda:',reply_markup=keyboard1)
             id_command[group_id]=1
         elif txt=="/ban":
             list1=[]
-            for elem in dizionario:
-                list1.append([elem])
+            for elem in array:
+                if array[elem] == "":
+                    list1.append([elem])
             print(list1)
             keyboard1 = ReplyKeyboardMarkup(keyboard=list1)
             bot.sendMessage(group_id, 'Digitare il codice della domanda:',reply_markup=keyboard1)
@@ -110,32 +115,36 @@ def on_chat_message(msg):
             id_command[group_id]=2
         elif txt == "/list":
             stringa=""
-            for elem in dizionario:
-                if stringa == "":
-                    stringa=elem
-                else:
-                    stringa+=",\n"+elem
+            for elem in array:
+                if array[elem]=="":
+                    if stringa == "":
+                        stringa=elem
+                    else:
+                        stringa+=",\n"+elem
             if stringa !="":
                 bot.sendMessage(chat_id, stringa)
             else:
                 bot.sendMessage(chat_id, "Lista vuota")
         elif txt == "/ban_list":
             stringa=""
-            for elem in ban:
-                if stringa == "":
-                    stringa=elem
-                else:
-                    stringa+=",\n"+elem
+            for elem in array:
+                if array[elem] == 'BANNED':
+                    if stringa == "":
+                        stringa=elem
+                    else:
+                        stringa+=",\n"+elem
+                    print(stringa)
             if stringa !="":
-                bot.sendMessage(chat_id, stringa)
+                bot.sendMessage(group_id, stringa)
             else:
-                bot.sendMessage(chat_id, "Lista vuota")
+                bot.sendMessage(group_id, "Lista vuota")
         elif chat_id in id_command:
             if id_command[chat_id]==1:
-                if txt in dizionario:
-                    question=txt
-                    bot.sendMessage(chat_id,"Digitare la risposta:")
-                    id_command[chat_id]=4
+                if txt in array:
+                    if array[txt]=="":
+                        question=txt
+                        bot.sendMessage(chat_id,"Digitare la risposta:")
+                        id_command[chat_id]=4
                 else:
                     bot.sendMessage(chat_id,"Error: Question not found")
             elif id_command[chat_id]==2:
@@ -145,26 +154,22 @@ def on_chat_message(msg):
                 bot.sendMessage(chat_id, 'Bug segnalato')
                 del id_command[chat_id]
             elif id_command[chat_id]==3:
-                if txt in dizionario:
-                    if txt in dizionario:
-                        bot.sendMessage(chat_id,"La domanda '"+txt+"' é stata bannata")
-                        dizionario.remove(question)
-                        txt = txt.replace(":","@")
-                        txt += ": BANNED"
-                        conn.send(txt.encode())
-                        del id_command[chat_id]
-                    else:
-                        bot.sendMessage(chat_id,"Error: Question not found")
+                if txt in array:
+                    bot.sendMessage(chat_id,"La domanda '"+txt+"' é stata bannata")
+                    array[txt]="BANNED"
+                    txt = txt.replace(":","@")
+                    txt += ": BANNED"
+                    conn.send(txt.encode())
+                    del id_command[chat_id]
                 else:
                     bot.sendMessage(chat_id,"Error: String not found")
                     del id_command[chat_id]
             elif id_command[chat_id]==4:
                 bot.sendMessage(chat_id,"La risposta a '"+question+"' é '"+txt+"'")
-                dizionario.remove(question)
+                array[question]=txt
                 question = question.replace(":","@")
                 txt = txt.replace(":","@")
                 txt = question+": "+txt
-                ban.append(question)
                 conn.send(txt.encode())
                 del id_command[chat_id]
         threadLock.release()
@@ -185,24 +190,22 @@ def ricevo() :
         msg=conn.recv(4096)
         x=msg.decode()
         threadLock.acquire()
-        if x[0] in dizionario:
+        if x in array:
             print("Error: String found")
         else :
             bot.sendMessage(-1001143270084, msg.decode())
             print("Il messaggio inviato è "+msg.decode())
-            dizionario.append(x)
+            array[x]=""
         threadLock.release()
 
 ShellThread=InputThread()
 ShellThread.start()
+
 with open('data.txt','r') as json_file:
-    data = json.load(json_file)
-    for i in range (0,len(data)):
-        for p in data[str(i)] :
-            if p['a'] == '':
-                dizionario.append(p['q'].replace("#","'").replace("$",'"'))
-            if p['a'] == 'BANNED':
-                ban.append(p['q'].replace("#","'").replace("$",'"'))
+    data=json.load(json_file)
+    for elem in data:
+        array[elem]=data[elem]["a"]
+
 print("Connected")
 ricevo()
 ShellThread.join()
