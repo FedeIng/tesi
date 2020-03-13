05f81acee6e8a9c5b78fbe80264a592068fe9d4e
FedeIng <56321831+FedeIng@users.noreply.github.com>
2020-03-13 02:32:26 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -9,3 +9,19 @@ def match(msg,command,chat_type,bot_name):
         return True
     print(4)
     return False
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor="callback_query")
+    if from_id not in id_command:
+        id_command.append(from_id)
+    bot.sendMessage(from_id, 'Bot in manutenzione, riprovare prossimamente')
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if chat_id not in id_command:
+        id_command.append(chat_id)
+    if content_type == 'text':
+        txt=msg['text'].lower()
+        for elem in array:
+            if match(txt,elem,chat_type,bot_name):
+                bot.sendMessage(chat_id, 'Bot in manutenzione, riprovare prossimamente')
