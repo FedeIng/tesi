100a96ff6d9b1ede24b9919a79e0c3ca20ce1447
FedeIng <56321831+FedeIng@users.noreply.github.com>
2020-09-29 16:05:47 +0200

    Add files via upload

--- bot_class.py
+++ bot_class.py
@@ -4,11 +4,11 @@ class Bot:
 
     def __init__(self,token,message=lambda msg : None,query=lambda msg : None):
         self.token=token
-        self.bot=telepot.Bot(token)
+        self.bot_instance=telepot.Bot(token)
         self.bot.message_loop({'chat':message,'callback_query':query})
 
     def get_bot(self):
-        return self.bot
+        return self.bot_instance
 
     def get_token(self):
         return self.token
