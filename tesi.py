9f87e39e7ba29d735ca7491a6b027889a22401f5
FedeIng <56321831+FedeIng@users.noreply.github.com>
2020-09-30 14:06:34 +0200

    Delete tesi.py

--- tesi.py
+++
@@ -1,4 +0,0 @@
-if message["chat"]["type"]=="private":
-    print("message['from']['id'] and message['chat']['id'] are equal")
-else:
-    print("message['from']['id'] and message['chat']['id'] are not equal")
