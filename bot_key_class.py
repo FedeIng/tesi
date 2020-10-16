ab437b7bb9a1f51cc8e5a8679deb055d4d5e8872
FedeIng <56321831+FedeIng@users.noreply.github.com>
2020-10-16 22:53:55 +0200

    Delete bot_key_class.py

--- bot_key_class.py
+++
@@ -1,9 +0,0 @@
-import telepot
-from database_class import Database
-
-class BotKey:
-    class Singleton:
-
-        def __init__(self):
-            self.database=Database()
-            self.key_id=self.database.get_key_id()
